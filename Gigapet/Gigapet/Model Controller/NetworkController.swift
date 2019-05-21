13727db2b57edec69da75e74ef9c1f60f4070ab2
Alex <alexcc17@gmail.com>
2019-05-21 18:51:28 -0400

    Add login function

--- Gigapet/Gigapet/Model Controller/NetworkController.swift
+++ Gigapet/Gigapet/Model Controller/NetworkController.swift
@@ -24,6 +24,7 @@ enum NetworkError: Error {
 class NetworkController {
     
     private let baseURL = URL(string: "https://giga-back-end.herokuapp.com/api")!
+    var bearer: Bearer?
     
     func mySignUp(with user: User, completion: @escaping (Error?) -> Void){
         //get url/end points
@@ -50,6 +51,61 @@ class NetworkController {
             }
             if let error = error {
                 print("Error in the data task function of the signup functon: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            completion(nil)
+            }.resume()
+    }
+    
+    
+    func logIn(with user: User, completion: @escaping (Error?) -> Void){
+        //get url
+        let url = baseURL.appendingPathComponent("users/login")
+        
+        //create urlRequest
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let je = JSONEncoder()
+        do {
+            let jsonData = try je.encode(user)
+            request.httpBody = jsonData
+        } catch  {
+            print("Error encoding user logging in: \(error.localizedDescription)")
+            completion(error)
+            return
+        }
+        
+        //now that we have the request set up we can run urlsesson
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                //we have a problem if it doesn't == 200
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                print("Error making loggin network task: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            
+            //we do want the data because the api states that it will send us our token
+            guard let data = data else {
+                print("error in the data secion of the login data task: \(NSError())")
+                completion(NSError())
+                return
+            }
+            
+            //decode data into our bearer
+            let jd = JSONDecoder()
+            do {
+                //the data we get back is the bearer so we are going to put that in itself and decode itself
+                self.bearer = try jd.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding the data into our bearer: \(error.localizedDescription)")
                 completion(error)
                 return
             }
