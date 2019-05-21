185b47c718a2a453899f87774a0fba073da7fdec
Alex <alexcc17@gmail.com>
2019-05-21 19:04:32 -0400

    Edit login

--- Gigapet/Gigapet/View Controllers/LoginViewController.swift
+++ Gigapet/Gigapet/View Controllers/LoginViewController.swift
@@ -11,9 +11,34 @@ import SwiftyJWT
 
 class LoginViewController: UIViewController {
 
+    // MARK: - Constants
+    
+    let nc = NetworkController()
+
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
+    
     @IBAction func loginBtnPressed(_ sender: UIButton) {
+        // Validate required fields are not empty
+        if (usernameTextField.text?.isEmpty)! ||
+            (passwordTextField.text?.isEmpty)! {
+            // Display Alert message and return
+            displayMessage(userMessage: "All fields are required to be filled in")
+            return
+        }
+        
+        // Not correct
+        let user = User(name: usernameTextField.text!, password: passwordTextField.text!, email: "NA")
+        
+        nc.logIn(with: user) { (error) in
+            if let error = error {
+                print("HERE", error)
+                return
+            }
+            DispatchQueue.main.async {
+                self.performSegue(withIdentifier: "Login Segue", sender: self)
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,4 +57,18 @@ class LoginViewController: UIViewController {
     }
     */
 
+    func displayMessage(userMessage:String) -> Void {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: "Alert", message: userMessage, preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+                // Code in this block will trigger when OK button tapped.
+                print("Ok button tapped")
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+            alertController.addAction(OKAction)
+            self.present(alertController, animated: true, completion:nil)
+        }
+    }
 }
--- Gigapet/Gigapet/View Controllers/SignupViewController.swift
+++ Gigapet/Gigapet/View Controllers/SignupViewController.swift
@@ -61,20 +61,18 @@ class SignupViewController: UIViewController {
         }
     }
     
-    // MARK: - Functions
-
     func displayMessage(userMessage:String) -> Void {
         DispatchQueue.main.async {
-                let alertController = UIAlertController(title: "Alert", message: userMessage, preferredStyle: .alert)
-                let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
-                    // Code in this block will trigger when OK button tapped.
-                    print("Ok button tapped")
-                    DispatchQueue.main.async {
-                            self.dismiss(animated: true, completion: nil)
-                    }
+            let alertController = UIAlertController(title: "Alert", message: userMessage, preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+                // Code in this block will trigger when OK button tapped.
+                print("Ok button tapped")
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: nil)
                 }
-                alertController.addAction(OKAction)
-                self.present(alertController, animated: true, completion:nil)
+            }
+            alertController.addAction(OKAction)
+            self.present(alertController, animated: true, completion:nil)
         }
     }
 }
